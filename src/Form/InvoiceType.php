2ae46c7ab7395191c185f12a939e6a6880d8820a
hrobben <hrobben@robusoft.nl>
2023-06-30 09:59:36 +0200

    Push deze week, einde juni. connectie vanuit dbase -> module in controller gegevens ophalen en doorsturen naar view (twig).

--- src/Form/InvoiceType.php
+++ src/Form/InvoiceType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Invoice;
+use App\Entity\InvoiceRow;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,26 @@ class InvoiceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('moment')
+            ->add('moment', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => 'Datum start',
+                'html5' => true,
+                // adds a class that can be selected in JavaScript
+                'attr' => ['class' => 'js-datepicker'],
+            ])
             ->add('user')
+            ->add('invoiceRows', CollectionType::class, [
+               // 'entry_type' => InvoiceRow::class,
+                'entry_options' => array('label' => false),
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+                'required' => false,
+                'attr' => array(
+                    'class' => 'my-selector',
+                ),
+                'by_reference' => false,
+            ])
         ;
     }
 
